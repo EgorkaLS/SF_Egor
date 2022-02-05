472e5fc9773a55b0523f3f313d16f88157b6ac04
EgorkaLS <cmn.sel@gmail.com>
2022-02-06 00:13:59 +0400

    Final

--- Prog_0/game.py
+++ Prog_0/game.py
@@ -10,19 +10,27 @@ def random_predict(number:int=1) -> int:
         number (int, optional): Загаданное число. Defaults to 1.
 
     Returns:
-        int: Число попыток
+        int: Число попыток до отгадывания числа
     """
 
-    count = 0
+    count = 0   #начальная инициализация счётчика попыток 
+    lo_pred = 1 # нижняя граница диапазона загадываемых чисел
+    hi_pred=101 # верхняя граница диапазона загадываемых чисел (можно увеличить для расширения диапазона здесь и 47 стр.)
+    predict_number = np.random.randint(lo_pred, hi_pred) # предполагаемое число
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
+        if predict_number > number: 
+            hi_pred = predict_number # Снижаем верхнюю границу при неудачной попытке
+            predict_number = np.random.randint(lo_pred, predict_number)  
+        if predict_number < number:
+            lo_pred = predict_number  # завышаем нижнюю границу при неудачной попытке
+            predict_number = np.random.randint(predict_number, hi_pred)  
         if number == predict_number:
             break # выход из цикла, если угадали
     return(count)
 
-print(f'Количество попыток: {random_predict()}')
+#print(f'Количество попыток: {random_predict()}')
 
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
@@ -43,8 +51,8 @@ def score_game(random_predict) -> int:
 
     score = int(np.mean(count_ls)) # находим среднее количество попыток
 
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
+    print(f'Алгоритм угадывает число в среднем за: {score} попыток')
+    return (score)
 
 # RUN
 if name == "mine":
